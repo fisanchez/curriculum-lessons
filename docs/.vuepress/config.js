87e1095e493c90a7cb02bde1dc33a624e9eaf6ae
Fernando Sanchez <fernando.ivan.sanchez@outlook.com>
2020-09-25 16:34:45 -0400

    config.js code cleanup

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,16 @@
-const { description } = require('../../package')
+const { description } = require("../../package");
 
 module.exports = {
-  markdown:{
+  markdown: {
     lineNumbers: false,
-    extendMarkdown: md => {
-      md.use(require('markdown-it-task-lists'), {enabled: true})
-    }
+    extendMarkdown: (md) => {
+      md.use(require("markdown-it-task-lists"), { enabled: true });
+    },
   },
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'JSAC Curriculum Lessons',
+  title: "JSAC Curriculum Lessons",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -22,9 +22,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
   /**
@@ -34,52 +37,51 @@ module.exports = {
    */
   themeConfig: {
     smoothScroll: true,
-    logo: 'https://static.wixstatic.com/media/b27321_7bd0b0b0993143d1aace7c1509dde80c~mv2.png/v1/fill/w_638,h_290,al_c,q_85,usm_0.66_1.00_0.01/jsac-color.webp',
-    repo: '',
+    logo:
+      "https://static.wixstatic.com/media/b27321_7bd0b0b0993143d1aace7c1509dde80c~mv2.png/v1/fill/w_638,h_290,al_c,q_85,usm_0.66_1.00_0.01/jsac-color.webp",
+    repo: "",
     editLinks: false,
-    docsDir: '',
-    editLinkText: '',
+    docsDir: "",
+    editLinkText: "",
     lastUpdated: false,
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Student Notes', link: '/student-notes/'},
+      { text: "Home", link: "/" },
+      { text: "Student Notes", link: "/student-notes/" },
     ],
     sidebar: {
-      '/student-notes/':[
+      "/student-notes/": [
         {
-          title: 'Student Notes',
+          title: "Student Notes",
           collapsable: true,
           children: [
-            '',
-            'wk-02-vars-functions',
-            'wk-03-dom-01',
-            'wk-04-arrays-objects-01',
-            'wk06-loops-01'
-          ]
-        }
+            "",
+            "variables-1.md",
+            "wk-02-vars-functions",
+            "wk-03-dom-01",
+            "wk-04-arrays-objects-01",
+            "wk06-loops-01",
+          ],
+        },
       ],
-      '/instructor-notes/': [
+      "/instructor-notes/": [
         {
-          title: 'Instructor Notes',
+          title: "Instructor Notes",
           collapsable: true,
-          children:[
-            '',
-            'wk-01-setup',
-            'wk-02-vars-and-functions',
-            'wk-03-the-dom-01',
-            'wk-04-arrays-objects-01',
-            'wk06-loops-01'
-          ]
-        }
+          children: [
+            "",
+            "wk-01-setup",
+            "wk-02-vars-and-functions",
+            "wk-03-the-dom-01",
+            "wk-04-arrays-objects-01",
+            "wk06-loops-01",
+          ],
+        },
       ],
-    }
+    },
   },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
+  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
 };
